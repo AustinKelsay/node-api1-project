4627c66d89251e19d277880806fffe07ef937b90
AustinKelsay <austinkelsay@yahoo.com>
2020-04-23 17:25:54 -0500

    Added middleware and tested server.post with insomnia

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 let users = [
-    {id: "1", name: "Jane Doe"}
-    {id: "2", name: "John Doe"}
+    {id: "1", name: "Jane Doe"},
+    {id: "2", name: "John Doe"},
     {id: "3", name: "Jim Doe"}
 ]
 
--- server.js
+++ server.js
@@ -1,12 +1,49 @@
 const express = require("express")
+const db = require("./database")
 
 //creates our server instance
 const server = express() 
+
+// Allows our API to parse request bodies that are JSON into usable objects
+server.use(express.json())
 
 //express comes with routing unlike raw http so we can define how our server will respond based on the endpoint
 //Here we define our route before we deal with any req/res
 server.get("/", (req, res) => {
     res.json({ message: "hello world" })
+})
+
+server.get("/users", (req, res) => {
+    const users = db.getUsers()
+    res.json(users)
+})
+
+server.get("/users/:id", (req, res) => {
+    const userId = req.params.id
+    const user = db.getUserById(userId)
+
+    if(user) {
+        res.json(user)
+    } else {
+        res.status(404).json({
+            message: "user not found",
+        })
+    }
+})
+
+server.post("/users", (req, res) => {
+    if (!req.body.name) {
+        return res.status(400).json({
+            message: "Need a user name"
+        })
+    }
+    const newUser = db.createUser({
+        id: 4,
+        name: req.body.name,
+    })
+    //status 201 is for confirming post success
+    res.status(201).json(newUser)
+
 })
 
 server.listen(6969, () => {
